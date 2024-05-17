772ab190225cbb4bee98fe1046df596b789bf280
Alex <alwx162@gmail.com>
2024-05-17 10:01:41 -0500

    env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRoutes = require('./routes/auth.js')
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
@@ -10,6 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/',(req,res)=>{
-  
+app.get('/', (req, res) => {
+  res.send('hola mundo')
 })
+
+app.use('/auth',authRoutes)
+
+app.listen(PORT, () =>
+  console.log(`Server running ${PORT}`)
+)
